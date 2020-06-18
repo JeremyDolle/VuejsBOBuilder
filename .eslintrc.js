3b8ee0a0c4df2fc71ad8fcf01f2df5b3e1845bd3
jeremydolle <j.dolle@thecodingmachine.com>
2020-06-18 18:19:09 +0200

    add linter config

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'plugin:vue/essential',
+    'standard',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    parser: 'babel-eslint',
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'vue',
+  ],
+  rules: {
+    'comma-dangle': ['error', 'always-multiline'],
+  },
+}
