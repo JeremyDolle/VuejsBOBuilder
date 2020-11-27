c7ec31c97611e614c88617ec5d12232bda0beb0e
jeremydolle <j.dolle@thecodingmachine.com>
2020-11-27 09:06:16 +0100

    remove blog section and override first page

--- documentation/src/pages/index.js
+++ documentation/src/pages/index.js
@@ -12,28 +12,25 @@ const features = [
     imageUrl: 'img/undraw_docusaurus_mountain.svg',
     description: (
       <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        Simple JSON files to configure
       </>
     ),
   },
   {
-    title: 'Focus on What Matters',
+    title: 'Scalable',
     imageUrl: 'img/undraw_docusaurus_tree.svg',
     description: (
       <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
+        The solution provide basic screens but allows customization and specific dev
       </>
     ),
   },
   {
-    title: 'Powered by React',
+    title: 'Powered by Vue',
     imageUrl: 'img/undraw_docusaurus_react.svg',
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        Vue 2 -> Vue 3
       </>
     ),
   },
