6d3365992408a6f3b64dd0cf0008777a5881d7a9
jeremydolle <j.dolle@thecodingmachine.com>
2020-12-06 15:14:31 +0100

    fix

---
+++ documentation/.docusaurus/client-modules.js
@@ -0,0 +1,8 @@
+export default [
+  require("/Users/jeremydolle/Documents/PROJETS/PERSO/VuejsBOBuilder/documentation/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/jeremydolle/Documents/PROJETS/PERSO/VuejsBOBuilder/documentation/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/jeremydolle/Documents/PROJETS/PERSO/VuejsBOBuilder/documentation/node_modules/remark-admonitions/styles/infima.css"),
+  require("/Users/jeremydolle/Documents/PROJETS/PERSO/VuejsBOBuilder/documentation/node_modules/infima/dist/css/default/default.css"),
+  require("/Users/jeremydolle/Documents/PROJETS/PERSO/VuejsBOBuilder/documentation/node_modules/@docusaurus/theme-classic/src/prism-include-languages"),
+  require("/Users/jeremydolle/Documents/PROJETS/PERSO/VuejsBOBuilder/documentation/src/css/custom.css"),
+];
